63a14888a2e2bb028cc1d89a9fae92eb58b34d3a
ItsPixL27 <sheth.neer27@gmail.com>
2024-11-01 19:30:36 +1100

    Nav bar started
    Started creating navigation bar, added basic styling, added another colour variable

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,24 @@
-import "./Header.css"
+import "./Header.css";
+import logo_dark from "../assets/logo_dark.svg";
 
 function Header() {
-  
+  return (
+    <nav>
+      <img src={logo_dark} alt="Neer's logo." className="logo"></img>
+      <ul className="nav-ul">
+        <li className="light-dark-switch">
+          <div>
+            <span className="material-symbols-outlined">dark_mode</span>
+            <span className="material-symbols-outlined">light_mode</span>
+          </div>
+        </li>
+        <li className="nav-ul_li">Home</li>
+        <li className="nav-ul_li">About</li>
+        <li className="nav-ul_li">Projects</li>
+        <li className="nav-ul_li">Contact</li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Header;
